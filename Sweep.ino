b971bd421575d8fc91f6c6739efd88d59d9076d9
20141105052 <xiaoods3on1@sina.com>
2015-12-02 18:28:40 +0800

    利用单片机控制旋转等
    更改单片机旋转角度和延时

--- Sweep.ino
+++ Sweep.ino
@@ -19,14 +19,30 @@ void setup() {
 }
 
 void loop() {
-  for (pos = 0; pos <= 120; pos += 1) { // goes from 0 degrees to 180 degrees
+  for (pos = 0; pos <= 30; pos += 50) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(10);                       // waits 15ms for the servo to reach the position
   }
-  for (pos = 120; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+  for (pos = 30; pos >= 60; pos -= 50) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(500);                       // waits 15ms for the servo to reach the position
+    delay(20);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 60; pos >= 90; pos -= 1000000) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(30);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 90; pos >= 120; pos -= 10) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(40);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 120; pos >= 150; pos -= 10) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(50);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 150; pos >= 180; pos -= 10) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(60);                       // waits 15ms for the servo to reach the position
   }
 }
 
